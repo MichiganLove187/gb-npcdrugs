64e0def80dc2a86de9de28b02ac799ed2671178f
MichiganLove187 <147991628+MichiganLove187@users.noreply.github.com>
2025-05-27 21:17:25 -0500

    Update config.lua

--- gb-drugsnpc/config.lua
+++ gb-drugsnpc/config.lua
@@ -1,15 +1,52 @@
 Config = {}
 
-Config.Cooldown = 10 * 1000
+Config.UseDrawText = true -- Set to true to use drawtext, false to use qb-target
+Config.InteractKey = 38 -- E key (for drawtext interaction)
+Config.InteractDistance = 2.0 -- Distance for interaction
+Config.Cooldown = 30 -- Cooldown in seconds between NPC interactions
 
-Config.DrugItems = {
-    ['cokebaggy']   = { label = "Bag of Coke",     min = 100, max = 200 },
-    ['crack_baggy'] = { label = "Bag of Crack",    min = 80,  max = 160 },
-    ['xtcbaggy']    = { label = "Bag of XTC",      min = 90,  max = 170 },
-    ['meth']        = { label = "Meth",            min = 120, max = 250 },
-    ['oxy']         = { label = "Oxy",             min = 100, max = 180 }
+Config.AttackWeapon = {
+    enabled = false, -- Set to false so npc use fist only
+    weapons = {
+        "WEAPON_KNIFE",
+        "WEAPON_BAT", 
+        "WEAPON_BOTTLE",
+        "WEAPON_HAMMER"
+    }
 }
 
-Config.DeclineChance    = 50
-Config.CallPoliceChance = 20
-Config.StealChance      = 10
+
+
+Config.DrugItems = {
+    ['cokebaggy'] = { 
+        label = "Bag of Coke",     
+        price = { min = 100, max = 200 },  -- Price for each single item
+        quantity = { min = 1, max = 38 }   -- How many can be sold in one sale
+    },
+    ['crack_baggy'] = { 
+        label = "Bag of Crack",    
+        price = { min = 80, max = 160 },
+        quantity = { min = 1, max = 4 }
+    },
+    ['xtcbaggy'] = { 
+        label = "Bag of XTC",      
+        price = { min = 90, max = 170 },
+        quantity = { min = 1, max = 6 }
+    },
+    ['meth'] = { 
+        label = "Meth",            
+        price = { min = 120, max = 250 },
+        quantity = { min = 1, max = 3 }
+    },
+    ['oxy'] = { 
+        label = "Oxy",             
+        price = { min = 100, max = 180 },
+        quantity = { min = 1, max = 8 }
+    }
+}
+
+
+Config.DeclineChance = 25
+Config.CallPoliceChance = 25
+Config.StealChance = 25
+Config.AttackChance = 25 -- set to 0 if you dont want peds to attack
